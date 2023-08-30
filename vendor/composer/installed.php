706380eecaeabe0bf08178f17f21fc6e8c4c1aa7
krystiano13 <crystianotv@gmail.com>
2023-08-30 09:49:29 +0200

    add autoload

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'krystian/image-uploader',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'e4679619ab4dfe93e8c3c845e335df255ff41889',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'krystian/image-uploader' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'e4679619ab4dfe93e8c3c845e335df255ff41889',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
