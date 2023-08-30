b25fd38f8ea5c584e4df469e7e8b4fec4884d544
krystiano13 <crystianotv@gmail.com>
2023-08-30 10:50:26 +0200

    add macy.js library

---
+++ classes/render.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+require_once '../vendor/autoload.php';
+
+use App\Connection;
+
+final class Render {
+    
+}
---
+++ gallery.js
@@ -0,0 +1,9 @@
+const macy = new Macy({
+  container: ".container-images",
+  columns: 3,
+  breakAt: {
+    992: {
+      colums: 1,
+    },
+  },
+});
