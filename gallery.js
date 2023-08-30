f07cb87ce98afb91cffbe0df01868fe9ebf9e657
krystiano13 <crystianotv@gmail.com>
2023-08-30 16:40:59 +0200

    improve some css styles and gallery script

--- gallery.js
+++ gallery.js
@@ -1,9 +1,17 @@
-const macy = new Macy({
-  container: ".images",
-  columns: 3,
-  breakAt: {
-    992: {
-      colums: 1,
+const images = document.querySelector(".images");
+
+if (images) {
+  const macy = new Macy({
+    container: ".images",
+    columns: 3,
+    margin: {
+      x: 6,
+      y: 6,
     },
-  },
-});
+    breakAt: {
+      992: {
+        colums: 1,
+      },
+    },
+  });
+}
